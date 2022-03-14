c7135cbf3e7a8c5f937252f67ba212d26b69173d
quang <quang@LAPTOP-1G7TTKR4>
2022-03-14 21:14:48 +0700

    Add project files.

---
+++ 1911065878_TranQuangTruong_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using _1911065878_TranQuangTruong_BigSchool.Models;
+using _1911065878_TranQuangTruong_BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1911065878_TranQuangTruong_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        
+    }
+}
---
+++ 1911065878_TranQuangTruong_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1911065878_TranQuangTruong_BigSchool.MvcApplication" Language="C#" %>
